cc057da5a2f0255867022d930ea6e9b729aaea63
Priten21 <pritenthakkar21042005@gmail.com>
2026-01-13 10:21:57 +0530

    deleted files

--- nest-Project/taskflow-backend/nest-learnings/src/middlewares/middleware2.ts
+++
@@ -1,4 +0,0 @@
-export default(req,res,next)=>{
-    console.log('reached middleware number 2');
-    next();
-}
--- nest-Project/taskflow-backend/nest-learnings/src/pipes/custom.pipes.ts
+++
@@ -1,9 +0,0 @@
-import { PipeTransform,Injectable,ArgumentMetadata } from "@nestjs/common";
-@Injectable()
-export class CustomPipe implements PipeTransform{
-    transform(value: any, metadata: ArgumentMetadata) {
-        console.log(value);
-        console.log(metadata.type);
-        return value;
-    }
-}
